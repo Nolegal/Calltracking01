de0c3289c59698d08b80cc98473a94a808b14e98
Nolegal <pike1988@i.ua>
2023-05-11 13:43:29 +0300

    add chromedriver manager

--- src/test/java/mytests/CallTracking.java
+++ src/test/java/mytests/CallTracking.java
@@ -3,13 +3,10 @@ package mytests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import javax.security.auth.login.Configuration;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
